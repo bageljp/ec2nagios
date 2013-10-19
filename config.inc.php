1e150e7fd00b90b1b6f5311166da3d4f62f49954
Keisuke Kadoyama <kadoyama@team-lab.com>
2013-10-20 03:35:31 +0900

    configに使用するパラメータを変数化 serviceのconfigを自動生成しないように変更 configに変更があった場合はnagiosを再起動出来るように変更

--- config.inc.php
+++ config.inc.php
@@ -10,10 +10,12 @@ CFCredentials::set(array(
 	'@default' => 'development'
 ));
 
-$nagios_config_path = '/etc/nagios/nagios.cfg';
-$ec2nagios_objects_directory = '/etc/nagios/ec2nagios';
+$program = 'icinga';
+$nagios_config_path = "/etc/{$program}/{$program}.cfg";
+$ec2nagios_objects_directory = "/etc/{$program}/conf.d/ec2nagios";
 $ec2nagios_config_filename = 'ec2nagios.cfg';
 $ec2nagios_tag_key = 'EC2Nagios';
+$tmp_directory = "/tmp";
 
 $regions = array(
 	AmazonEC2::REGION_US_E1,
@@ -27,3 +29,8 @@ $regions = array(
 );
 
 $use_public_dns = false;
+$use_host = 'linux-server';
+$use_service = 'local-service';
+$use_contactgroup = 'admins';
+$create_service_cfg = false;
+$service_restart = true;
--- ec2nagios.php
+++ ec2nagios.php
@@ -7,6 +7,7 @@ $ec2 = new AmazonEC2();
 
 # Add EC2Nagios configuration directory to nagios.cfg
 $nagios_config = @file_get_contents($nagios_config_path);
+/* not auto configuration
 $directory_configuration = "cfg_dir={$ec2nagios_objects_directory}";
 if (strpos($nagios_config, $directory_configuration) === false) {
 	$nagios_config = preg_replace('/(.*)cfg_dir=([^\n]*)/ms', '$0' . "\n\n{$directory_configuration}", $nagios_config);
@@ -14,6 +15,7 @@ if (strpos($nagios_config, $directory_configuration) === false) {
 		$nagios_config .= "\n{$directory_configuration}\n";
 	file_put_contents($nagios_config_path, $nagios_config);
 }
+*/
 
 # Make EC2Nagios objects directory if not exists
 if (!file_exists($ec2nagios_objects_directory))
@@ -36,7 +38,7 @@ foreach ($regions as $region) {
 		foreach ($reservationItem->instancesSet->children() as $instanceItem) {
 			if ($instanceItem->instanceState->name != 'running')
 				continue;
-			$node_dns = $instanceItem->dnsName;
+			$node_dns = $use_public_dns ? $instanceItem->dnsName : $instanceItem->privateIpAddress;
 			$node_ip = $use_public_dns ? $instanceItem->dnsName : $instanceItem->privateIpAddress;
 			$node_name = null;
 			$hostgroup = null;
@@ -44,14 +46,14 @@ foreach ($regions as $region) {
 				$tag_key = $tag->key->to_string();
 				$tag_value = $tag->value->to_string();
 				if (strcasecmp($tag_key, 'Name') === 0) {
-					$node_name = $tag_value;
+					$node_name = $use_public_dns ? $tag_value : $tag_value . '.' . $instanceItem->privateIpAddress;
 				}
 				if (strcasecmp($tag_key, $ec2nagios_tag_key) === 0) {
 					$hostgroup = $tag_value;
 				}
 			}
 			if ($hostgroup) {
-				$ec2nagios_config .= create_host_config($node_dns, $node_name, $node_ip);
+				$ec2nagios_config .= create_host_config($node_dns, $node_name, $node_ip, $use_host, $use_contactgroup);
 				$hostgroup_members[$hostgroup][] = $node_dns;
 			}
 		}
@@ -63,26 +65,41 @@ foreach ($hostgroup_members as $hostgroup => $members) {
 	$ec2nagios_config .= create_hostgroup_config($hostgroup, $members);
 }
 
+copy("{$ec2nagios_objects_directory}/{$ec2nagios_config_filename}", "{$tmp_directory}/{$ec2nagios_config_filename}");
 file_put_contents("{$ec2nagios_objects_directory}/{$ec2nagios_config_filename}", $ec2nagios_config);
 
-# Create template hostgroup configuration if not exists
-$hostgroups = array_keys($hostgroup_members);
-foreach ($hostgroups as $hostgroup) {
-	$hostgroup_config_path = "{$ec2nagios_objects_directory}/{$hostgroup}.cfg";
-	if (!file_exists($hostgroup_config_path)) {
-		$hostgroup_config = create_hostgroup_config_template($hostgroup);
+# Create template hostgroup configuration
+if ($create_service_cfg) {
+	$hostgroups = array_keys($hostgroup_members);
+	foreach ($hostgroups as $hostgroup) {
+		$hostgroup_config_path = "{$ec2nagios_objects_directory}/service-{$hostgroup}.cfg";
+		$hostgroup_config = create_hostgroup_config_template($hostgroup, $use_service);
 		file_put_contents($hostgroup_config_path, $hostgroup_config);
 	}
 }
 
-function create_host_config($node_dns, $node_name, $node_ip) {
+# Service restart
+if ($service_restart) {
+        $compare = array();
+        $diff_list = array("{$ec2nagios_objects_directory}/{$ec2nagios_config_filename}", "{$tmp_directory}/{$ec2nagios_config_filename}");
+	foreach($diff_list as $value){
+	    $file = fopen($value, "r");
+	    $data = fread($file, filesize($value));
+	    fclose($file);
+	    array_push($compare, $data);
+	}
+	if ($compare[0] !== $compare[1]) service_restart($program, $nagios_config_path);
+}
+
+function create_host_config($node_dns, $node_name, $node_ip, $use_host, $use_contactgroup) {
 
 	$ec2nagios_config = <<<EOT
 define host{
-	use             linux-server
+	use             {$use_host}
         host_name       {$node_dns}
         alias           {$node_name}
         address         {$node_ip}
+        contact_groups  {$use_contactgroup}
         }
 
 EOT;
@@ -108,13 +125,13 @@ EOT;
 
 }
 
-function create_hostgroup_config_template($hostgroup) {
+function create_hostgroup_config_template($hostgroup, $use_service) {
 
 	$ec2nagios_config = <<<EOT
 # You can edit following lines for "{$hostgroup}" hostgroup
 
 define service{
-        use                     local-service
+        use                     {$use_service}
         hostgroup_name          {$hostgroup}
         service_description     PING
         check_command           check_ping!100.0,20%!500.0,60%
@@ -124,4 +141,11 @@ EOT;
 
 	return $ec2nagios_config;
 
+}
+
+function service_restart($program, $nagios_config_path) {
+	exec("{$program} -v {$nagios_config_path}", $output, $retval);
+	if ($retval === 0) exec("/etc/init.d/{$program} restart", $output, $retval);
+
+	return $retval;
 }
